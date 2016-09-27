53ce5dcad63870778d245e8ffd0ac0506b0cedfb
Alexander Ogurtsov <iscander@gmail.com>
2016-09-27 16:51:06 +0300

    Use variables and environment settings

--- day7/code/wiki/constants.go
+++ day7/code/wiki/constants.go
@@ -1,7 +1,23 @@
 package main
 
-const (
-	PagesPath     = "pages"
-	TemplatesPath = "templates"
-	StaticPath    = "static"
+import (
+	"os"
 )
+
+const (
+	DefaultPagesPath     = "pages"
+	DefaultTemplatesPath = "templates"
+	envTemplatesPath     = "TEMPLATES_PATH"
+	envPagesPath         = "PAGES_PATH"
+	StaticPath           = "static"
+)
+
+var TemplatesPath = envOrDefault(envTemplatesPath, DefaultTemplatesPath)
+var PagesPath = envOrDefault(envPagesPath, DefaultPagesPath)
+
+func envOrDefault(envar, def string) string {
+	if s := os.Getenv(envar); len(s) > 0 {
+		return s
+	}
+	return def
+}
