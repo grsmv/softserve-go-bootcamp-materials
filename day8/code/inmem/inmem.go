1ab517e89efbfbe971d292c113c6250e28fd76bc
Alexander Ogurtsov <iscander@gmail.com>
2016-09-29 13:24:49 +0300

    Mockup for inmemory database

---
+++ day8/code/inmem/inmem.go
@@ -0,0 +1,41 @@
+package inmem
+
+import (
+	"fmt"
+	"sync"
+)
+
+var errNotFound = fmt.Errorf("Key not found")
+
+func (im inMem) set(key string, value interface{}) {
+	im.Lock()
+	im.storage[key] = value
+	im.Unlock()
+}
+func (im inMem) get(key string) (interface{}, error) {
+	im.RLock()
+	result, ok := im.storage[key]
+	im.RUnlock()
+	if !ok {
+		return nil, errNotFound
+	}
+	return result, nil
+}
+func (im inMem) delete(key string) {
+	im.Lock()
+	delete(im.storage, key)
+	im.Unlock()
+}
+
+type inMem struct {
+	sync.RWMutex
+	storage map[string]interface{}
+}
+
+//Return new inmem storage
+func New() *inMem {
+	im := inMem{
+		storage: make(map[string]interface{}),
+	}
+	return &im
+}
