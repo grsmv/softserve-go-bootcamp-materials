e95d4b0b398e7c84b0f25ef3a8f627dd4a59ebea
Alexander Ogurtsov <iscander@gmail.com>
2016-09-29 16:25:29 +0300

    Interface to return multiple string values from DB. Types conversions implemented

--- day8/code/inmem/db.go
+++ day8/code/inmem/db.go
@@ -1,6 +1,10 @@
 package inmem
 
-import "time"
+import (
+	"strconv"
+	"strings"
+	"time"
+)
 
 type inMemDB struct {
 	dbs map[string]*inMem
@@ -44,6 +48,26 @@ func (memDB *inMemDB) Get(dbname string, key string, receiver interface{}) error
 	return err
 }
 
+func (memDB *inMemDB) GetMulti(dbname string, pattern string) ([]string, error) {
+	db, ok := memDB.dbs[dbname]
+	if !ok {
+		return nil, errNotExists
+	}
+	response, err := db.getMulti(pattern)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]string, 0, len(response))
+	var str string
+	for _, val := range response {
+		str, err = stringFromRecord(val)
+		if err == nil {
+			result = append(result, str)
+		}
+	}
+	return result, nil
+}
+
 func (memDB *inMemDB) CloseDB(dbname string) error {
 	db, ok := memDB.dbs[dbname]
 	if !ok {
@@ -57,17 +81,67 @@ func (memDB *inMemDB) CloseDB(dbname string) error {
 }
 
 func intFromRecord(val interface{}) (int, error) {
-	return 0, errNotImplemented
+	switch val.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return val.(int), nil
+	case float32, float64:
+		return int(val.(float64)), nil
+	case string:
+		return strconv.Atoi(val.(string))
+	case bool:
+		if val.(bool) {
+			return 1, nil
+		}
+		return 0, nil
+	default:
+		return 0, errNotImplemented
+	}
 }
 
 func boolFromRecord(val interface{}) (bool, error) {
-	return false, errNotImplemented
+	switch val.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return val.(int) != 0, nil
+	case float32, float64:
+		return val.(float64) != 0.0, nil
+	case string:
+		return strings.ToLower(strings.Trim(val.(string), "\r\n\t ")) == "true", nil
+	case bool:
+		return val.(bool), nil
+	default:
+		return false, errNotImplemented
+	}
 }
 
 func floatFromRecord(val interface{}) (float64, error) {
-	return 0.0, errNotImplemented
+	switch val.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return float64(val.(int)), nil
+	case float32, float64:
+		return val.(float64), nil
+	case string:
+		return strconv.ParseFloat(val.(string), 64)
+	case bool:
+		if val.(bool) {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	default:
+		return 0.0, errNotImplemented
+	}
 }
 
 func stringFromRecord(val interface{}) (string, error) {
-	return "", errNotImplemented
+	switch val.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return strconv.Itoa(val.(int)), nil
+	case float32, float64:
+		return strconv.FormatFloat(val.(float64), 'f', -1, 64), nil
+	case string:
+		return val.(string), nil
+	case bool:
+		return strconv.FormatBool(val.(bool)), nil
+	default:
+		return "", errNotImplemented
+	}
 }
